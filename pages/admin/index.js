05b26dfd7ed8ffa03e4884c6ae1c4ef33e76782f
ProyectoAura <sebastianchungo@gmail.com>
2025-04-07 17:01:11 -0300

    Add files via upload

---
+++ pages/admin/index.js
@@ -0,0 +1,10 @@
+// pages/admin/index.js
+
+export default function Admin() {
+  return (
+    <div style={{ color: "white", padding: "2rem" }}>
+      <h1>Panel de Administrador</h1>
+      <p>En construcción...</p>
+    </div>
+  );
+}
